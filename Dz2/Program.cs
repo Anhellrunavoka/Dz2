e20d41abc556fafbd8ac100b94c431492dedc155
Anhelina Gordieieva <angelina.gordeeva10@gmail.com>
2024-12-10 19:12:36 +0200

    Add project files.

---
+++ Dz2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using RandLib;
+Rand random = new Rand();
+Console.WriteLine("Input how many random numbers is generate: ");
+int count_num = Convert.ToInt32(Console.ReadLine());
+random.Numbers(count_num);
+Console.WriteLine("Input length random string is generate: ");
+int count_str = Convert.ToInt32(Console.ReadLine());
+random.Strings(count_str);
---
+++ RandLib/Rand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace RandLib
+{
+    public class Rand
+    {
+        public void Numbers(int num) {
+            Random rand=new Random();
+            for (int i = 0; i < num; i++)
+            {
+                int rannum = rand.Next(0, 1000);
+                Console.WriteLine("Random number is " + rannum);
+            }
+        }
+        public void Strings(int length)
+        {
+            Random rand = new Random();
+            string symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#№%:;^&?/|*(){}[].<,>";
+            string ryadok ="";
+            for (int i = 0; i < length; i++) { 
+                int rn=rand.Next(0, symbols.Length);
+                ryadok += symbols[rn];
+            }
+            Console.WriteLine("Random string is " + ryadok);
+        }
+    }
+}
